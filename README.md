511d44d883a86bc04fc930e9ad75324036e5a039
TheRealRavager <iskandar@u.nus.edu>
2019-01-20 11:47:47 +0800

    Test sometimes prints, providing non photo for photo no longer crashes. Partial implementation of edit report

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ Current Bugs:
 
 1. UnhandledPromiseRejectionWarning: Error: 400: Bad Request: message text is empty (Appears when user interacts with bot, does not affect performance at all)
 
-2. 'Yes' & 'No' inline buttons only able to display with an unprinted 'TEST' string (go and see hard to describe)
+2. 'Yes' & 'No' inline buttons only able to display with an unprinted 'TEST' string (go and see hard to describe) (SOMETIMES THE TEST PRINTS OUT WHAT THE ACTUAL MOTHER OF)
 
 3. Inline keyboards still available until the user leaves the bot's page
-
-4. If user is prompted to send photo but sends anything else, bot crashes
 
 To-do Functionality:
 
@@ -16,6 +14,6 @@ To-do Functionality:
 
 2. Data verification (proper matric numbers etc)
 
-3. Ability to edit report (right now any mistake made requires the user to re-do the report from the start)
+3. Ability to edit report (right now any mistake made requires the user to re-do the report from the start) (PARTIAL IMPLEMENT)
 
 4. Ability for the user to check the status of his current reports (to see what stage of the repair-work has been completed)
--- wizardo.js
+++ wizardo.js
@@ -30,6 +30,8 @@ let fault_photo_id = ''
 let fault_desc = ''
 let fault_loc = ''
 let reportKey = ''
+let corrector = ''
+let reviewer_first = true
 
 //will someone explain to me what composer does (should be decide when to move to next step)
 const stepHandler = new Composer()
@@ -61,12 +63,18 @@ const superWizard = new WizardScene('super-wizard',
     ctx.reply('Thanks! Could you please send me a photo of the fault?')
     return ctx.wizard.next()
   },
-  (ctx) => {  //5
+  function photo_acceptor(ctx) { //checks photo
+    try { //Try and catch here to prevent error when user sends non-photo message //BUT HOW TO MAKE IT LOOP BACK TO THE START
     fault_photo = ctx.message.photo.pop().file_id
     fault_photo_id = (ctx.message.photo.pop()['file_id'])
+    }
+    catch(err) {
+    ctx.reply('That is not a photo! Please try again!')
+    return ctx.wizard.selectStep(4) // Loops in the same
+    }
     ctx.reply('Fault photo saved! Thank you! Could you please describe the fault to me?')
     return ctx.wizard.next()
-  },
+    },
   (ctx) => {  //6
     fault_desc = ctx.message.text
     ctx.reply('Oh no, that sounds bad! Could you let me know where the fault is located at?',
@@ -74,8 +82,11 @@ const superWizard = new WizardScene('super-wizard',
     )
     return ctx.wizard.next()
   },
-  (ctx) => {  //7
-    fault_loc = ctx.message.text
+  function reviewer(ctx) {  //7
+    if (reviewer_first) {
+        fault_loc = ctx.message.text
+    }
+    reviewer_first = false
     ctx.replyWithPhoto(fault_photo,
     {caption: "Alright! Could you verify that all the details are correct?: \n\n" +
               "Name: " + user_name + "\n" +
@@ -93,9 +104,9 @@ const superWizard = new WizardScene('super-wizard',
   (ctx) => {  //8
     let correct = ctx.message.text.toLowerCase()  //Prompt user to review the report and respond with 'yes' or 'no'
     if (correct == "no") {  //8a
-        ctx.reply("Then you waste my time for what. 😡")
-        ctx.scene.leave()
-        return bot.use(session())
+        ctx.reply('Okay! Which part did you get wrong?',
+            Markup.keyboard(["Fullname", "Matric No.", "Photo", "Fault Description", "Fault Location"]).oneTime().resize().extra())
+        return ctx.wizard.next()
     } else {  //8b
       ctx.reply("Upload successful! Thank you for the report!")
       // sends the data to firebase
@@ -112,6 +123,33 @@ const superWizard = new WizardScene('super-wizard',
       reportKey = report.key;
     return ctx.scene.leave()
     }
+  },
+  function editor_sayer(ctx) {
+    corrector = ctx.message.text
+    ctx.reply('Alright! Go ahead and send me the correct ' + ctx.message.text + '!')
+    return ctx.wizard.next()
+  },
+  function editor_doer(ctx) {
+    switch (corrector) {
+        case "Fullname":
+            user_name = ctx.message.text
+            ctx.reply(user_name)
+            break
+        case "Matric No.":
+            user_matric = ctx.message.text
+            break
+        case "Photo": //NO ERROR CATCHER
+            fault_photo = ctx.message.photo.pop().file_id
+            fault_photo_id = (ctx.message.photo.pop()['file_id'])
+            break
+        case "Fault Description":
+            fault_desc = ctx.message.text
+            break
+        case "Fault Location": //USER HAS TO INPUT TEXT INSTEAD OF PREDEFINED LOCATIONS
+            fault_loc = ctx.message.text
+            break
+    }
+    return ctx.wizard.selectStep(6)
   }
 )
 
